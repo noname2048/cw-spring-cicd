761e5533183cef7d2512286b6ce8f2002695dc52
Sungwook Choi <sungwook.csw@gmail.com>
2023-08-11 20:02:54 +0900

    ci: 실패하는 assertion 삽입

---
+++ src/test/java/com/example/demo/HelloControllerTest.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+
+class HelloControllerTest {
+
+    @Test
+    void root() {
+        assert(false);
+    }
+
+    @Test
+    void health() {
+    }
+}
