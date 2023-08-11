1e533f309c105f6e5d1d168c996a4beb7a455480
Sungwook Choi <noname2048@users.noreply.github.com>
2023-08-11 20:50:13 +0900

    [#1] 새로운 CI 워크플로우 생성
    * ci: create ci workflow
    
    * ci: do-test 에서 ci-test로 이름변경
    
    * ci: 실패하는 assertion 삽입
    
    * ci: 성공하는 assert 문으로 변경

---
+++ src/test/java/com/example/demo/HelloControllerTest.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+
+class HelloControllerTest {
+
+    @Test
+    void root() {
+        assert(true);
+    }
+
+    @Test
+    void health() {
+    }
+}
